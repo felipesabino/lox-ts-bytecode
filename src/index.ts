4ae17e3a4122f9edb14ec014de2cda21370df5d7
Felipe Sabino <felipe@sabino.me>
2020-08-13 19:10:58 -0300

    added repl and running from source file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,35 @@
+import readline from 'readline';
+import fs from 'fs';
 
-(() => {
-  process.argv.forEach(function (val, index, array) {
-    console.log(index + ': ' + val);
+let runPrompt = () => {
+  let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
   });
-  console.log('it works');
-})();
+
+  rl.prompt();
+  rl.on('line', function(line){
+      run(line);
+      rl.prompt();
+  })
+}
+
+let runFile = (path: string) => {
+  let file = fs.readFileSync(path);
+
+  run(file);
+}
+
+let run = (source: string | Buffer) => {
+  console.log(`code: ${source}`);
+}
+
+if (process.argv) {
+  if(process.argv.length == 2) runPrompt();
+  else if(process.argv.length == 3) runFile(process.argv[2]);
+  else {
+    console.error("usage: tslox [script]");
+    process.exit(64);
+  }
+}
